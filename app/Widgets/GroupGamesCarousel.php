3bb0b04296fb7710bc64598466e74f626e5d4170
Лапочкин Р.А <ralapochkin@activebc.ru>
2019-09-18 16:39:18 +0300

    Fix deleted tournament

--- app/Widgets/GroupGamesCarousel.php
+++ app/Widgets/GroupGamesCarousel.php
@@ -20,13 +20,13 @@ class GroupGamesCarousel extends AbstractWidget
     {
         $regularGames = GroupGameRegular::with(['homeTeam.team', 'awayTeam.team', 'tournament'])
             ->whereNotNull('playedAt')
-            ->whereNotNull('deletedAt')
+            ->whereNull('deletedAt')
             ->orderByDesc('playedAt')
             ->take(10)
             ->get();
         $playoffGames = GroupGamePlayoff::with(['homeTeam.team', 'awayTeam.team', 'tournament', 'playoffPair'])
             ->whereNotNull('playedAt')
-            ->whereNotNull('deletedAt')
+            ->whereNull('deletedAt')
             ->orderByDesc('playedAt', 'desc')
             ->take(10)
             ->get();
