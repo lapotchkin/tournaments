b24b573516d0f1b0ac52e26a0796f01802a932a9
Лапочкин Р.А <ralapochkin@activebc.ru>
2019-09-18 14:56:28 +0300

    Migrations

--- database/migrations/2019_09_18_141954_modify_personal_game_playoff.php
+++ database/migrations/2019_09_18_141954_modify_personal_game_playoff.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyPersonalGamePlayoff extends Migration
+class ModifyPersonalGamePlayoff2 extends Migration
 {
     /**
      * Run the migrations.
