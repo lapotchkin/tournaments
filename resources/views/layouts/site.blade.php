dad416ad39719ee715a417347155472c32403ce3
Лапочкин Р.А <ralapochkin@activebc.ru>
2019-10-22 11:50:28 +0300

    Fix layout for mobiles

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -2,6 +2,8 @@
 <html lang="ru">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('title')Киберспортивная лига</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @section('script')
