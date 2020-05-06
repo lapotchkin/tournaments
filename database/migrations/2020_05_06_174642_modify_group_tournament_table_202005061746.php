2a178a359a6b022f7dabdbc91c282656603eb767
Лапочкин Р.А <ralapochkin@activebc.ru>
2020-05-06 17:58:35 +0300

    tournaments-44 Players rating at team page

---
+++ database/migrations/2020_05_06_174642_modify_group_tournament_table_202005061746.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyGroupTournamentTable202005061746 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groupTournament', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groupTournament', function (Blueprint $table) {
+            //
+        });
+    }
+}
